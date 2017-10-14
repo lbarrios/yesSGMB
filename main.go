c87cfea8b71f79f7f6b5ddfff2603bfb8c1cf78b
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-10-14 17:04:04 -0300

    flags are global vars; using log.Fatalf to format

--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"flag"
 	"log"
 	"github.com/lbarrios/yesSGMB/cartridge"
-	"fmt"
+)
+
+var (
+	romFile = flag.String("rom", "test.gb", "Path to rom file")
 )
 
 func main() {
 	// Parsing the parameters
-	romFile := flag.String("rom", "test.gb", "Path to rom file")
 	flag.Parse()
 
 	// Loading the cartridge data
 	_, err := cartridge.NewCartridge(*romFile)
 	if err != nil {
-		log.Fatal(fmt.Sprintf("ERROR: %s",err))
+		log.Fatalf("ERROR: %s",err)
 	}
 }

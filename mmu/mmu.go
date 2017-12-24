bbe73bd9c84bb779391923e22142235ab1b7484e
Ezequiel Barrios <ezequiel@barrios.com.ar>
2017-12-24 18:13:39 -0300

    Added mmu read switch case

--- mmu/mmu.go
+++ mmu/mmu.go
@@ -26,27 +26,69 @@ func (mmu *mmu) LoadCartridge(cart *cartridge.Cartridge) {
 	mmu.cartridge = cart
 }
 
+const (
+	ROM_BANK_0_16KB             = 0x0000
+	SWITCHABLE_ROM_BANK_16KB    = 0x4000
+	VIDEO_RAM_8KB               = 0x8000
+	SWITCHABLE_RAM_BANK_8KB     = 0xA000
+	INTERNAL_RAM_8KB            = 0xC000
+	ECHO_8KB_INTERNAL_RAM       = 0xE000
+	SPRITE_ATTRIB_MEMORY_OAM    = 0xFE00
+	EMPTY_BUT_UNUSABLE_FOR_IO_1 = 0xFEA0
+	IO_PORTS                    = 0xFF00
+	EMPTY_BUT_UNUSABLE_FOR_IO_2 = 0xFF4C
+	INTERNAL_RAM                = 0xFF80
+	INTERRUPT_ENABLE_REGISTER   = 0xFFFF
+)
+
 func (mmu *mmu) ReadByte(address types.Address) byte {
-	if address.High < 0x80 {
-		if address.High < 0x40 {
-			// ROM Bank 0
-			return mmu.cartridge.MBC.Read(address)
-		} else {
-			// ROM Bank 1 (switchable)
-			return mmu.cartridge.MBC.Read(address)
-		}
-	} else {
-		if address.High >= 0xA0 && address.High < 0xC0 {
-			return mmu.cartridge.MBC.Read(address)
-		}
-		if address.High >= 0xC0 {
-			if address.High < 0xE0 {
-				// GB Internal RAM
-			}
-			if address.High >= 0xE0 && address.High < 0xFE {
-				// GB Internal RAM shadow
-			}
-		}
+	if address.AsWord() < SWITCHABLE_ROM_BANK_16KB {
+		// ROM_BANK_0_16KB
+		return mmu.cartridge.MBC.Read(address)
+	}
+	if address.AsWord() >= SWITCHABLE_ROM_BANK_16KB && address.AsWord() < VIDEO_RAM_8KB {
+		// SWITCHABLE_ROM_BANK_16KB
+		return mmu.cartridge.MBC.Read(address)
+	}
+	if address.AsWord() >= VIDEO_RAM_8KB && address.AsWord() < SWITCHABLE_RAM_BANK_8KB {
+		// VIDEO_RAM_8KB
+		log.Printf("MMU: Attemping to read from unimplemented address %x (VIDEO_RAM_8KB)", address.AsWord())
+	}
+	if address.AsWord() >= SWITCHABLE_RAM_BANK_8KB && address.AsWord() < INTERNAL_RAM_8KB {
+		// SWITCHABLE_RAM_BANK_8KB
+		return mmu.cartridge.MBC.Read(address)
+	}
+	if address.AsWord() >= INTERNAL_RAM_8KB && address.AsWord() < ECHO_8KB_INTERNAL_RAM {
+		// INTERNAL_RAM_8KB
+		log.Printf("MMU: Attemping to read from unimplemented address %x (INTERNAL_RAM_8KB)", address.AsWord())
+	}
+	if address.AsWord() >= ECHO_8KB_INTERNAL_RAM && address.AsWord() < SPRITE_ATTRIB_MEMORY_OAM {
+		// ECHO_8KB_INTERNAL_RAM
+		log.Printf("MMU: Attemping to read from unimplemented address %x (ECHO_8KB_INTERNAL_RAM)", address.AsWord())
+	}
+	if address.AsWord() >= SPRITE_ATTRIB_MEMORY_OAM && address.AsWord() < EMPTY_BUT_UNUSABLE_FOR_IO_1 {
+		// SPRITE_ATTRIB_MEMORY_OAM
+		log.Printf("MMU: Attemping to read from unimplemented address %x (SPRITE_ATTRIB_MEMORY_OAM)", address.AsWord())
+	}
+	if address.AsWord() >= EMPTY_BUT_UNUSABLE_FOR_IO_1 && address.AsWord() < IO_PORTS {
+		// EMPTY_BUT_UNUSABLE_FOR_IO_1
+		log.Printf("MMU: Attemping to read from unimplemented address %x (EMPTY_BUT_UNUSABLE_FOR_IO_1)", address.AsWord())
+	}
+	if address.AsWord() >= IO_PORTS && address.AsWord() < EMPTY_BUT_UNUSABLE_FOR_IO_2 {
+		// IO_PORTS
+		log.Printf("MMU: Attemping to read from unimplemented address %x (IO_PORTS)", address.AsWord())
+	}
+	if address.AsWord() >= EMPTY_BUT_UNUSABLE_FOR_IO_2 && address.AsWord() < INTERNAL_RAM {
+		// EMPTY_BUT_UNUSABLE_FOR_IO_2
+		log.Printf("MMU: Attemping to read from unimplemented address %x (EMPTY_BUT_UNUSABLE_FOR_IO_2)", address.AsWord())
+	}
+	if address.AsWord() >= INTERNAL_RAM && address.AsWord() < INTERRUPT_ENABLE_REGISTER {
+		// INTERNAL_RAM
+		log.Printf("MMU: Attemping to read from unimplemented address %x (INTERNAL_RAM)", address.AsWord())
+	}
+	if address.AsWord() >= INTERRUPT_ENABLE_REGISTER {
+		// INTERRUPT_ENABLE_REGISTER
+		log.Printf("MMU: Attemping to read from unimplemented address %x (INTERRUPT_ENABLE_REGISTER)", address.AsWord())
 	}
 	log.Printf("MMU: Attemping to read from invalid address %x", address.AsWord())
 	return 0x00
